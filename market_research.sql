152b05e3b4e558f67f679307beaca80696389599
VieriNice <escamillavieri@gmail.com>
2024-10-18 12:42:59 -0500

    Updated Market Research

--- market_research.sql
+++ market_research.sql
@@ -1,8 +1,19 @@
-#most recent information. Shows death toll for 2016 by mutiple causes#
+#most recent information. Shows death toll by mutiple causes#
 
 SELECT leadingdeathcause.`Year`, leadingdeathcause.`113 Cause Name`, leadingdeathcause.`Cause Name`, leadingdeathcause.`State`, leadingdeathcause.Deaths
 FROM leadingdeathcause
-WHERE leadingdeathcause.`113 Cause Name` = 'All Causes' AND leadingdeathcause.`Year` = '2016' AND leadingdeathcause.`State` = 'United States';
+WHERE leadingdeathcause.`113 Cause Name` = 'All Causes' AND leadingdeathcause.`State` = 'United States';
+
+#query shows highest year with the most deaths#
+SELECT  MAX(leadingdeathcause.`year`) AS 'Highest Death Rate Year'
+FROM leadingdeathcause ;
+
+#This query helps me find ALE of the last 50+ years #
+SELECT AVG(`Average Life Expectancy (Years)`) AS 'Average Expectancy Throught 50+ Years', Sex, `Year`
+FROM deathratebyage
+WHERE Sex = 'Both Sexes'
+GROUP BY `Average Life Expectancy (Years)`, Sex, `Year`
+ORDER BY 'Average Expectancy Through 50+ Years' DESC;
 
 
 
@@ -13,12 +24,15 @@ SELECT `Year`, `Cause Name`, Deaths, State
 FROM leadingdeathcause
 WHERE `Cause Name` = 'cancer' AND `State` = 'United States';
 
+
+
 #With this query, i am able to determine the death caused by accidents#
 SELECT * FROM market_research.leadingdeathcause;
 
 SELECT `Year`, `Cause Name`, Deaths, State
 FROM leadingdeathcause
 WHERE `Cause Name` LIKE '%Unintentional%' AND `State` = 'United States';
+
 
 #This query will help me determine the deaths that were caused by strokes#
 SELECT * FROM market_research.leadingdeathcause;
@@ -27,8 +41,15 @@ SELECT `Year`, `Cause Name`, Deaths, State
 FROM leadingdeathcause
 WHERE `Cause Name` = 'Stroke' AND `State` = 'United States';
 
+SELECT AVG(Deaths) 
+FROM leadingdeathcause;
+
+
+
 #This Query helped me find the year the people died, what their life expectancy was, their age adjusted death rate, and the cause of their death#
 SELECT deathratebyage.`Year`, deathratebyage.Sex, deathratebyage.`Average Life Expectancy (Years)`, deathratebyage.`Age-adjusted Death Rate`, leadingdeathcause.`Cause Name`
 FROM deathratebyage 
 JOIN leadingdeathcause ON deathratebyage.`Year` =leadingdeathcause.`Year`
 GROUP BY deathratebyage.`Year`, deathratebyage.Sex, deathratebyage.`Average Life Expectancy (Years)`, deathratebyage.`Age-adjusted Death Rate`, leadingdeathcause.`Cause Name`;
+
+#This query helps me find the 
